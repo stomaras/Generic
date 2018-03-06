bbfc8b374daad2a516b62bba3603375538d7ccd0
stomaras <sptomaras@yahoo.gr>
2018-03-06 17:09:52 +0200

    Create ListDemo.java

---
+++ ListDemo.java
@@ -0,0 +1,12 @@
+package PersonProject;
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PersonList pList= new PersonList();
+		pList.print();
+
+	}
+
+}
