6f287c078dbb68d7f168b51064d0e6990f74e32f
stomaras <sptomaras@yahoo.gr>
2018-03-18 14:19:46 +0200

    Update ListDemo.java

--- ListDemo.java
+++ ListDemo.java
@@ -3,7 +3,6 @@ package PersonProject;
 public class ListDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		PersonList pList= new PersonList();
 		pList.print();
 
